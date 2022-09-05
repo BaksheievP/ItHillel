7db6df0acc4685dd5eefbcca637b4a6025ef6bfa
Pavlo Baksheiev <pbaksheev95@gmail.com>
2022-09-05 14:59:01 +0300

    DZ4 final

--- DZ4/Script.js
+++ DZ4/Script.js
@@ -1,43 +1,42 @@
+const calculation = {
+    "+": function(a, b) {return a + b},
+    "-": function(a, b) {return a - b},
+    "*": function(a, b) {return a * b},
+    "/": function(a, b) {return a / b},
+};
 
-const operandA = getMathoperand('Enter operand : +, -, +, /')
-const numbersA = getNumbers('How amount numbers are you use?')
-console.log(numbersA)
+const operand = getValidatedValue('Enter operand : +, -, *, /', chooseMathoperator)
+console.log('your operand is -', operand)
+const counter = getValidatedValue('How amount numbers do you use?', validateNumber)
+console.log('you use -', counter,'numbers')
 
-const numbersB = getNumbers('Enter first number')
 
+let result = getValidatedValue('Enter number', validateNumber)
+console.log(result)
+for(let i = 0; i < counter - 1; i++){
 
-for(let i = 0; i <= numbersB; i++){
-    console.log(i) 
+    const numberItem = getValidatedValue('Enter number', validateNumber)
+    console.log(numberItem)
+    result = calculation[operand](result, +numberItem) 
+    
 }
-
-
-
-
-
-
-
+console.log('calculation result -',result)
 
 
 function chooseMathoperator(operator){
-    return operator === '+'|| operator === '-'|| operator === '*'|| operator === '/' 
+    return !(operator === '+'|| operator === '-'|| operator === '*'|| operator === '/')
     }
 
-function getMathoperand(label){
-    let value 
-while(!chooseMathoperator(value)) {
-        value = prompt(label);
-       }
-    return label
-}
-
-function getNumbers(amountnumbers){
-    let num 
-    while(validateNumber(num)){
-        num = prompt(amountnumbers)
-    }
-    return num
-}
 
 function validateNumber(number){
     return number === null || number === '' || isNaN(number);
     }
+
+    function getValidatedValue(question, validationFunc){
+        let value
+        while(validationFunc(value)) {
+            value = prompt(question);
+        }
+        return value
+    }
+
