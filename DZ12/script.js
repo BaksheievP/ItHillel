12718d967540053c41fe740f3124e596651e3898
Pavlo Baksheiev <pbaksheev95@gmail.com>
2022-10-04 18:11:36 +0300

    dz11

---
+++ DZ12/script.js
@@ -0,0 +1,12 @@
+'use sctict'
+function Calc(number) {
+    this.set = (value) => number = value,
+    this.sum = (value) => number = number + value,
+    this.mult = (value) => number = number * value,
+    this.div  =(value) => number = number / value,
+    this.sub = (value) => number = number - value   
+}
+
+
+let calc = new Calc()
+
