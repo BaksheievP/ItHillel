5b38555934855fc66af9312904fbd2a5cab68e92
Pavlo Baksheiev <pbaksheev95@gmail.com>
2022-10-04 10:57:20 +0300

    test

--- DZ11/script.js
+++ DZ11/script.js
@@ -1,27 +1,31 @@
 const DELETE_BTN_CLASS = 'delete-btn'
+const TODO_ITEM_CLASS = 'todo-item';
+const TODO_CLASS ='done'
 const TODO_ITEM_SELECTOR = '.todo-item'
-const INVALID_INPUT_CLASS = '.invalid-input'
+const INVALID_INPUT_CLASS = 'invalid-input'
 const toDoList = document.querySelector('#todolist')
 const todoTemplate = document.querySelector('#Tdtemplate').innerHTML;
-const todoEl = document.querySelector('#todo');
+const todoInput = document.querySelector('input');
 const todoForm = document.querySelector('#todoform');
 const todoTable = document.querySelector('#container');
 const btnOnClick = document.querySelector('#addBntClick');
 
 let todoElList = [
-    { toDo: 'do homework', id: 1 },
-    { toDo: 'do mistakes', id: 2 },
-    { toDo: 'do diff things', id: 3 },
+    { toDo: 'do homework', id: 1, isDone : false },
+    { toDo: 'do mistakes', id: 2, isDone : true },
+    { toDo: 'do diff things', id: 3, isDone : false},
 ];
 
 todoForm.addEventListener('submit', onFormSubmit);
 toDoList.addEventListener('click', onToDoListClick);
-todoForm.addEventListener('input', onFormElementInput);
+toDoList.addEventListener('click',  onFormElementInput)
+
 
 init();
 
 function init() {
     renderList(todoElList);
+    
 }
 
 function onFormSubmit(e) {
@@ -29,17 +33,24 @@ function onFormSubmit(e) {
     const newTodo = getToDoValues();
     addToDo(newTodo);
     resetFormData();
+    
 }
 function onToDoListClick(e) {
     if (e.target.classList.contains(DELETE_BTN_CLASS)) {
         const toDoId = getToDoId(e.target)
         deleteToDoId(toDoId)
     }
+    if (e.target.classList.contains(TODO_ITEM_CLASS)) {
+        const toDoId = getToDoId(e.target)
+        toggleTodo(toDoId)
+    }
+
 }
 
 
 function onFormElementInput(e) {
     validateInput(e.target);
+    
 }
 
 function renderList(list) {
@@ -47,16 +58,17 @@ function renderList(list) {
 
 }
 
-function generateTodoHtml({ toDo, id }) {
+function generateTodoHtml({ toDo, id, isDone }) {
     return todoTemplate.replaceAll('{{ToDo}}', toDo)
         .replaceAll('{{id}}', id)
+        .replaceAll('{{classlayout}}', isDone? TODO_CLASS : '' )
        
 
 }
 
 function getToDoValues() {
     return {
-        toDo: todoEl.value
+        toDo: todoInput.value
     }
 }
 
@@ -67,6 +79,7 @@ function getToDoId(el) {
 function addToDo(todo) {
 
     todo.id = Date.now();
+    todo.isDone = false;
     todoElList.push(todo);
     renderList(todoElList)
 }
@@ -85,10 +98,17 @@ function validateInput(input) {
 }
 
 function resetValidation(input) {
-   input.classList.remove(INVALID_INPUT_CLASS)
+    input.classList.remove(INVALID_INPUT_CLASS)
   
 }
 
 function resetFormData() {
-    todoEl.value = '';
+    todoInput.value = '';
+}
+
+function toggleTodo(id){
+    const todo = todoElList.find((item) => item.id === id)
+    todo.isDone = !todo.isDone;
+    renderList(todoElList)
+
 }
