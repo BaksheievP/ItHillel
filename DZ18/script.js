c5360c075447024fb8c9e52a66056b6d941f47e1
Pavlo Baksheiev <pbaksheev95@gmail.com>
2022-11-20 22:06:36 +0200

    dz19

---
+++ DZ18/script.js
@@ -0,0 +1,30 @@
+const URL = 'https://jsonplaceholder.typicode.com/photos?_limit=10';
+const template = $('#photoItemTemplate').html();
+const $photosContainer = $('#photos');
+
+init();
+
+function init(){
+    fetchPhotos();
+}
+
+function fetchPhotos(){
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        renderPhotos(data);
+      })
+}
+
+function renderPhotos(list){
+    const photos = list.map(getPhotoElement);
+    $photosContainer.append(photos);
+
+    $photosContainer.find('a').simpleLightbox({
+        fileExt: '',    
+    });
+}
+
+function getPhotoElement(photo){
+    return $(interpolate(template, photo))
+}
